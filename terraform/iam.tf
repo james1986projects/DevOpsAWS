b13d172dda93bdab908010dde30b393767cd4ea5
James Peckitt <james_peckitt86@hotmail.com>
2025-07-14 21:21:04 +0100

    Update: secure AWS deployment with HTTPS, Route 53, IAM roles

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,16 +1,14 @@
-#iam.tf
-
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRole"
+      Action = "sts:AssumeRole",
       Principal = {
         Service = "ecs-tasks.amazonaws.com"
-      }
-      Effect = "Allow"
+      },
+      Effect = "Allow",
       Sid    = ""
     }]
   })
@@ -25,29 +23,38 @@ resource "aws_iam_role" "ecs_task_app_role" {
   name = "ecsTaskAppRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRole"
+      Action = "sts:AssumeRole",
       Principal = {
         Service = "ecs-tasks.amazonaws.com"
-      }
-      Effect = "Allow"
+      },
+      Effect = "Allow",
       Sid    = ""
     }]
   })
 }
 
 resource "aws_iam_policy" "dynamodb_policy" {
-  name        = "DynamoDBAccessPolicy"
-  description = "Allows access to DynamoDB table"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["dynamodb:PutItem", "dynamodb:GetItem", "dynamodb:UpdateItem"]
-      Resource = "*"
-    }]
+  name = "ecs-dynamodb-access-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ],
+        Resource = aws_dynamodb_table.flask_data.arn
+      }
+    ]
   })
+
+  # Ensure the DynamoDB table exists before the policy is created
+  depends_on = [aws_dynamodb_table.flask_data]
 }
 
 resource "aws_iam_policy_attachment" "dynamodb_policy_attach" {
---
+++ terraform/route53.tf
@@ -0,0 +1,20 @@
+#route53.tf
+
+# Look up your hosted zone for devopsjames.com
+data "aws_route53_zone" "primary" {
+  name         = "devopsjames.com"
+  private_zone = false
+}
+
+# Create A record (alias) pointing to your ALB
+resource "aws_route53_record" "root_domain" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "devopsjames.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.flask_alb.dns_name
+    zone_id                = aws_lb.flask_alb.zone_id
+    evaluate_target_health = true
+  }
+}
