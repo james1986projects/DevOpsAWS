5f6d5850d7e25081c195c8cb632fc6837965a72e
James Peckitt <james_peckitt86@hotmail.com>
2025-07-28 09:15:15 +0100

    Clean up old files from environment folders

--- terraform/environments/prod/main.tf
+++
@@ -1,9 +0,0 @@
-#main.tf test environement (use `../..` to point to root terraform dierctory infra)
-
-module "app_stack" {
-  source = "../../"
-
-  acm_certificate_arn = var.acm_certificate_arn
-  image_tag           = var.image_tag
-  environment         = var.environment
-}
--- terraform/environments/test/main.tf
+++
@@ -1,7 +0,0 @@
-module "app_stack" {
-  source = "../../"
-
-  acm_certificate_arn = var.acm_certificate_arn
-  image_tag           = var.image_tag
-  environment         = var.environment
-}
