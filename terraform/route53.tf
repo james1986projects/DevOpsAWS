73ea45ed496997d4a57e35679a8542127d72ee52
James Peckitt <james_peckitt86@hotmail.com>
2025-07-28 22:12:10 +0100

    Deploy prod environment: ALB, HTTPS, ECS, Route53 DNS, ECR, ACM cert

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -18,3 +18,15 @@ resource "aws_route53_record" "root_domain" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "root_domain_root" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "devopsjames.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.flask_alb.dns_name
+    zone_id                = aws_lb.flask_alb.zone_id
+    evaluate_target_health = true
+  }
+}
