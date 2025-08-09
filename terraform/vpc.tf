a91d0db944fe2f6099e31a6289228a1ab5c669c9
James Peckitt <james_peckitt86@hotmail.com>
2025-08-09 11:52:29 +0100

    Restore vpc.tf (default VPC, ECS SG on port 5000)

---
+++ terraform/vpc.tf
@@ -0,0 +1,36 @@
+# vpc.tf
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name = "${var.environment}-ecs-sg"
+  description = "Allow inbound port 5000 from ALB"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   tags = {
+    Name = "${var.environment}-ecs-sg"
+  }
+}
